f9d612d6395ebfecd7673c01cec0e94720ba6977
Izabela <iszybiak.kontakt@gmail.com>
2024-11-13 16:13:22 +0100

    reading-file

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from openai import OpenAI
 
+def read_article(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        article_text = file.read()
+    return article_text
 
-API_KEY= open("API_KEY", "r").read().strip()
-client = OpenAI(api_key=API_KEY)
-
-completions = client.chat.completions.create(
-    model="gpt-4o-mini",
-    messages=[{
-        "role": "user",
-        "content": "Say this is a test",
-    }],
-)
-
-print(completions.choices[0].message.content)
+# API_KEY= open("API_KEY", "r").read().strip()
+# client = OpenAI(api_key=API_KEY)
+#
+# completions = client.chat.completions.create(
+#     model="gpt-4o-mini",
+#     messages=[{
+#         "role": "user",
+#         "content": "Say this is a test",
+#     }],
+# )
+#
+# print(completions.choices[0].message.content)
